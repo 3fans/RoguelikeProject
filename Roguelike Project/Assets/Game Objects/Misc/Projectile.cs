0c83cad0a89e642037332c25424c65c751710f23
3fans <143848172+3fans@users.noreply.github.com>
2024-04-06 11:33:08 -0400

    projectile direction

---
+++ Roguelike Project/Assets/Game Objects/Misc/Projectile.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour, IDirection
+{
+    public CircleCollider2D circleCollider;
+    public Rigidbody2D RB;
+    public Animator animator;
+    public PlayerScript Player;
+    private float projSpeed = 2;
+
+    public IDirection.Direction8 direction8 { get; set; }
+
+    public IDirection.Direction8 VectorToDirection(Vector2 vector)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void Awake()
+    {
+        circleCollider = GetComponent<CircleCollider2D>();
+        animator = GetComponent<Animator>();
+        RB = GetComponent<Rigidbody2D>();
+        Player = FindFirstObjectByType<PlayerScript>();
+
+        direction8 = Player.direction8;
+
+        RB.rotation = Direction8ToFloat(direction8);
+
+    }
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector2 _direction = Direction8ToVector2(direction8);
+        RB.velocity = new Vector2(_direction.x * projSpeed, _direction.y * projSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        circleCollider.
+    }
+
+    public float Direction8ToFloat(IDirection.Direction8 direction8)
+    {
+        switch (direction8)
+        {
+            case IDirection.Direction8.E:
+                return 0;
+
+            case IDirection.Direction8.NE:
+                return 45;
+ 
+            case IDirection.Direction8.N:
+                return 90;
+
+            case IDirection.Direction8.NW:
+                return 135;
+
+            case IDirection.Direction8.W:
+                return 180;
+
+            case IDirection.Direction8.SW:
+                return 225;
+
+            case IDirection.Direction8.S:
+                return 270;
+
+            case IDirection.Direction8.SE:
+                return 315;
+
+            default:
+                return 0;
+        }
+    }
+    public Vector2 Direction8ToVector2(IDirection.Direction8 direction8)
+    {
+        switch (direction8)
+        {
+            case IDirection.Direction8.E:
+                return new Vector2(1,0).normalized;
+
+            case IDirection.Direction8.NE:
+                return new Vector2(1, 1).normalized;
+
+            case IDirection.Direction8.N:
+                return new Vector2(0, 1).normalized;
+
+            case IDirection.Direction8.NW:
+                return new Vector2(-1, 1).normalized;
+
+            case IDirection.Direction8.W:
+                return new Vector2(-1, 0).normalized;
+
+            case IDirection.Direction8.SW:
+                return new Vector2(-1, -1).normalized;
+
+            case IDirection.Direction8.S:
+                return new Vector2(0, -1).normalized;
+
+            case IDirection.Direction8.SE:
+                return new Vector2(1, -1).normalized;
+
+            default:
+                return new Vector2(1, 0).normalized;
+        }
+    }
+
+}
